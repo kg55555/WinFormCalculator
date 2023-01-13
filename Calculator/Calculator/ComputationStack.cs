b58fa93bcc1d4b591baf37451e7461f318e3d22d
Kevin <kg25000@gmail.com>
2023-01-13 12:30:53 -0800

    Added stack class

---
+++ Calculator/Calculator/ComputationStack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class ComputationStack
+    {
+
+        public ComputationStack() { }
+
+        public double ProcessEquation(string equation)
+        {
+
+
+            return 0;
+        }
+
+    }
+}
