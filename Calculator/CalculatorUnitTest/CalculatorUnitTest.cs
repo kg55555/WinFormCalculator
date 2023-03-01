1144ed1334ff39d411f09335d532835080abbe6a
Kevin <kg25000@gmail.com>
2023-02-28 22:56:44 -0800

    Changed memory function

---
+++ Calculator/CalculatorUnitTest/CalculatorUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class CalculatorUnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+    }
+}
